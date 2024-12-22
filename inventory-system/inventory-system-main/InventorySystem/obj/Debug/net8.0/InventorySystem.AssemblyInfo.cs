464ae1ada9572c16eff25678c3381291c672e5a2
Edzra Macas <EdzraM7@gmail.com>
2024-12-22 22:43:55 +0800

    Log in and colors

--- inventory-system/inventory-system-main/InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
+++ inventory-system/inventory-system-main/InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5466845573753d1a256f9b6519363456669649c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
