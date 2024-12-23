1617a229e6b1f502b573f2ba5f940132cdb1fff8
Bengie Villesco <bvillesco@gmail.com>
2024-12-23 15:38:22 +0800

    remove in stocks tab

--- inventory-system/inventory-system-main/InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
+++ inventory-system/inventory-system-main/InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+464ae1ada9572c16eff25678c3381291c672e5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b791342d59f1080377fe1d97c29b2940f79e525b")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
